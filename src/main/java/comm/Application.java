d7c1ea38868e420b335b60938713cdc13f25152b
BioAngel <13910053205@139.com>
2018-08-16 00:46:50 +0800

    Update Application.java

--- src/main/java/comm/Application.java
+++ src/main/java/comm/Application.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("comm.mapper")
 public class Application {
